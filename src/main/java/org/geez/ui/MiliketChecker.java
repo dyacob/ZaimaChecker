3806e39e1b715fbbb4017e1852ac51172d59ce75
dyacob <yacob@geez.org>
2019-08-08 07:17:39 -0400

    updates for new UI in sync with DocxConver and Xliterator

--- src/main/java/org/geez/ui/MiliketChecker.java
+++ src/main/java/org/geez/ui/MiliketChecker.java
@@ -2,9 +2,8 @@ package org.geez.ui;
 
 
 import java.awt.Desktop;
-
 import java.io.File;
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,40 +12,40 @@ import org.geez.zaima.CheckMiliket;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.VPos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Tooltip;
-import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.embed.swing.SwingFXUtils;
 // import java.awt.Taskbar; java 9
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
  
 public final class MiliketChecker extends Application {
  
@@ -60,9 +59,11 @@ public final class MiliketChecker extends Application {
 	private boolean openOutput = true;
 	private boolean fix121 = false;
 	private List<File>  inputList = null;
-	private ProgressBar progressBar = null;
+	private List<File> inputFileList = null;
 	
 	private boolean recheck = false;
+	private static final String VERSION = "v0.4.0";
+	private final int APP_HEIGHT = 220, APP_WIDTH = 420;
 	
 	
     private static void configureFileChooser( final FileChooser fileChooser ) {      
@@ -87,9 +88,7 @@ public final class MiliketChecker extends Application {
            	listView.fireEvent(new ListView.EditEvent<Label>(listView, ListView.editCommitEvent(), label, i));
         	i++;
         }
-        listView.refresh();
-        // it seems no UI refresh happens until this handle method exits
-        // try { Thread.sleep(10000) ; } catch(Exception ex) {} ;    	
+        listView.refresh();    	
     }
     
     
@@ -99,43 +98,20 @@ public final class MiliketChecker extends Application {
         Image logoImage = new Image( ClassLoader.getSystemResourceAsStream("images/geez-org-avatar.png") );
         stage.getIcons().add( logoImage );
         // revisit for java9 to replace com.apple.eawt below: Taskbar.getTaskbar().setIconImage( logoImage );
-        final Label label = new Label( "ዜማ ምልእክት Checker" );
-
-
-        ComboBox<String> bookMenu = new ComboBox<String>();
+        // final Label label = new Label( "ዜማ ምልእክት Checker" );
+    	
         String osName = System.getProperty("os.name");
-        // System.out.println( osName );
+        String defaultFont = "Ebrima";
         if( osName.equals("Mac OS X") ) {
-        	bookMenu.setStyle("-fx-font: 12px \"Kefa\";");
-            bookMenu.getItems().addAll( ኹሉም, ድጓ, ጾመ_ድጓ, ምዕራፍ );       
-            bookMenu.setValue( ኹሉም );
-            label.setStyle("-fx-font: 24px \"Kefa\";");
-            
-            com.apple.eawt.Application.getApplication().setDockIconImage( SwingFXUtils.fromFXImage(logoImage, null) );      
-            
+        	defaultFont = "Kefa";
+            com.apple.eawt.Application.getApplication().setDockIconImage( SwingFXUtils.fromFXImage(logoImage, null) );            
         }
-        else if( osName.startsWith("Windows") ) {
-        	bookMenu.setStyle("-fx-font: 12px \"Ebrima\";");
-            bookMenu.getItems().addAll( ኹሉም, ድጓ, ጾመ_ድጓ, ምዕራፍ );       
-            bookMenu.setValue( ኹሉም );
-            label.setStyle("-fx-font: 24px \"Ebrima\";");
-        }
-        else {
-        	bookMenu.getItems().addAll( "All", "Digua" , "Tsome Digua" , "Me'eraf" );       
-        	bookMenu.setValue( "All" );
-        }
-        bookMenu.valueProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> ov, String oldSet, String newSet) {
-                miliketSet = newSet;
-            } 
-        });
         
 
         ListView<Label> listView = new ListView<Label>();
         listView.setEditable(false);
-        listView.setPrefHeight( 100 );
-        listView.setPrefWidth( 280 );
+        listView.setPrefHeight( APP_HEIGHT - 40 );
+        listView.setPrefWidth( APP_WIDTH - 60 );
         ObservableList<Label> data = FXCollections.observableArrayList();
         VBox listVBox = new VBox( listView );
         listView.autosize();
@@ -180,22 +156,35 @@ public final class MiliketChecker extends Application {
                     }
                 }
         );
+      
+        
+        CheckBox openFilesCheckbox = new CheckBox( "Open file(s) after hecking?");
+        openFilesCheckbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> ov,
+                Boolean old_val, Boolean new_val) {
+                    openOutput = new_val.booleanValue();
+            }
+        });
+        openFilesCheckbox.setSelected(true);
 
-
-        final Button openFilesButton  = new Button("Select Files...");
+ 
+        // progressBar = new ProgressBar();
+        // progressBar.setProgress( 0 );
+        final Menu fileMenu = new Menu("_File"); 
         final FileChooser fileChooser = new FileChooser();
         
-        openFilesButton.setOnAction(
+        // create menu items 
+        final MenuItem fileMenuItem1 = new MenuItem( "Select Files..." ); 
+        fileMenuItem1.setOnAction(
             new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(final ActionEvent e) {
                 	listView.getItems().clear();
-                	recheck = false;
                 	configureFileChooser(fileChooser);    
-                    inputList = fileChooser.showOpenMultipleDialog( stage );
+                    inputFileList = fileChooser.showOpenMultipleDialog( stage );
                     
-                    if( inputList != null ) {
-                    	for( File file: inputList) {
+                    if ( inputFileList != null ) {
+                    	for( File file: inputFileList) {
                     		Label rowLabel = new Label( file.getName() );
                     		data.add( rowLabel );
                     		Tooltip tooltip = new Tooltip( file.getPath() );
@@ -207,57 +196,117 @@ public final class MiliketChecker extends Application {
                 }
             }
         );
+        fileMenu.getItems().add( fileMenuItem1 ); 
+        fileMenu.getItems().add( new SeparatorMenuItem() );
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setOnAction(actionEvent -> Platform.exit());
+        fileMenu.getItems().add( exitMenuItem ); 
+        
+        
+        final Menu helpMenu = new Menu( "Help" );
+        final MenuItem aboutMenuItem = new MenuItem( "About" );
+        helpMenu.getItems().add( aboutMenuItem );
+        
+        aboutMenuItem.setOnAction(
+            new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(final ActionEvent e) {
+			        Alert alert = new Alert(AlertType.INFORMATION);
+			        alert.setTitle( "About Zaima Checker" );
+			        alert.setHeaderText( "Zaima Miliket Checker " + VERSION );
+			        
+			        FlowPane fp = new FlowPane();
+			        Label label = new Label( "Visit the project homepage on" );
+			        Hyperlink link = new Hyperlink("GitHub");
+			        fp.getChildren().addAll( label, link);
 
+			        link.setOnAction( (event) -> {
+	                    alert.close();
+	                    try {
+		                    URI uri = new URI( "https://github.com/dyacob/ZaimaCorrect/" );
+		                    desktop.browse( uri );
+	                    }
+	                    catch(Exception ex) {
+	                    	
+	                    }
+			        });
+
+			        alert.getDialogPane().contentProperty().set( fp );
+			        alert.showAndWait();
+                }
+            }
+        );
+        
+        Region bottomSpacer = new Region();
+        HBox.setHgrow(bottomSpacer, Priority.SOMETIMES);
+        HBox hbottomBox = new HBox( openFilesCheckbox, bottomSpacer, convertButton );
+        hbottomBox.setPadding(new Insets(4, 0, 4, 0));
+        hbottomBox.setAlignment( Pos.CENTER_LEFT );
+        VBox vbottomBox = new VBox( hbottomBox );
+        
+        // create a menubar 
+        MenuBar leftBar = new MenuBar(); 
+        Menu checkMenu = new Menu( "Check" );
+        RadioMenuItem checkItem1 = new RadioMenuItem( ድጓ );
+        RadioMenuItem checkItem2 = new RadioMenuItem( ጾመ_ድጓ );
+        RadioMenuItem checkItem3 = new RadioMenuItem( ምዕራፍ );
+        checkItem1.setStyle("-fx-font: 12px \"" + defaultFont + "\";");
+        checkItem2.setStyle("-fx-font: 12px \"" + defaultFont + "\";");
+        checkItem3.setStyle("-fx-font: 12px \"" + defaultFont + "\";");
+        checkItem1.setSelected(true);
+        checkItem2.setSelected(true);
+        checkItem3.setSelected(true);
+
+        checkMenu.getItems().addAll( checkItem1, checkItem2, checkItem3 );
+        Menu stripeMenu = new Menu( "Stripe" );
+        RadioMenuItem stripeItem1 = new RadioMenuItem( ድጓ );
+        RadioMenuItem stripeItem2 = new RadioMenuItem( ጾመ_ድጓ );
+        RadioMenuItem stripeItem3 = new RadioMenuItem( ምዕራፍ );
+        ToggleGroup stripeGroup = new ToggleGroup();
+        stripeItem1.setToggleGroup( stripeGroup );
+        stripeItem2.setToggleGroup( stripeGroup );
+        stripeItem3.setToggleGroup( stripeGroup );
+        stripeItem1.setStyle("-fx-font: 12px \"" + defaultFont + "\";");
+        stripeItem2.setStyle("-fx-font: 12px \""  + defaultFont + "\";");
+        stripeItem3.setStyle("-fx-font: 12px \""  + defaultFont + "\";");
+        stripeMenu.getItems().addAll( stripeItem1, stripeItem2, stripeItem3 );
+  
+
+        MenuItem fix121MenuItem = new RadioMenuItem( "Set \"1-2-1\" to \"centered\"?" );
+        fix121MenuItem.setOnAction( evt -> { fix121 = (fix121) ? false : true ; } );
+        
+        Menu optionsMenu = new Menu( "_Options" );
+        optionsMenu.getItems().addAll( checkMenu, fix121MenuItem, stripeMenu );
+        
+        // add menu to menubar 
+        leftBar.getMenus().addAll( fileMenu, optionsMenu );
+       
+        MenuBar rightBar = new MenuBar();
+        rightBar.getMenus().addAll( helpMenu );
+        Region spacer = new Region();
+        spacer.getStyleClass().add( "menu-bar" );
+        HBox.setHgrow(spacer, Priority.SOMETIMES);
+        HBox menubars = new HBox( leftBar, spacer, rightBar );
+        menubars.setAlignment( Pos.CENTER_LEFT );
         
         
-        CheckBox openFilesCheckbox = new CheckBox( "Open file(s) after\nchecking?");
-        openFilesCheckbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
-            public void changed(ObservableValue<? extends Boolean> ov,
-                Boolean old_val, Boolean new_val) {
-                    openOutput = new_val.booleanValue();
+        
+        final BorderPane rootGroup = new BorderPane();
+        rootGroup.setTop( menubars );
+        rootGroup.setCenter( listVBox );
+        rootGroup.setBottom( vbottomBox );
+        //rootGroup.setPadding( new Insets(8, 8, 8, 8) );
+        rootGroup.setPadding( new Insets(4, 8, 4, 8) );
+ 
+        Scene scene = new Scene(rootGroup, APP_WIDTH, APP_HEIGHT );
+        scene.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+                listView.setPrefHeight( Integer.parseInt(newSceneHeight.toString().split("\\.")[0] ) - 40);
             }
         });
-        openFilesCheckbox.setSelected(true);
-        
-        CheckBox fix121Checkbox = new CheckBox( "Set \"1-2-1\" to \"centered\"?");
-        fix121Checkbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
-            public void changed(ObservableValue<? extends Boolean> ov,
-                Boolean old_val, Boolean new_val) {
-                    fix121 = new_val.booleanValue();
-            }
-        });
-        fix121Checkbox.setSelected(false);
- 
-        // progressBar = new ProgressBar();
-        // progressBar.setProgress( 0 );
-
-        final GridPane inputGridPane = new GridPane();
-
-        GridPane.setConstraints(label, 0, 0, 2, 1);
-        GridPane.setConstraints(bookMenu, 0, 1);               GridPane.setConstraints(openFilesButton, 1, 1);
-        GridPane.setHalignment(bookMenu, HPos.LEFT);           GridPane.setHalignment(openFilesButton, HPos.RIGHT);
-
-        GridPane.setConstraints(listVBox, 0, 2, 2, 1);
-        GridPane.setConstraints(openFilesCheckbox, 0, 3);      GridPane.setConstraints(convertButton, 1, 3);
-        GridPane.setHalignment(openFilesCheckbox, HPos.LEFT);  GridPane.setHalignment(convertButton, HPos.RIGHT);
-        GridPane.setConstraints(fix121Checkbox, 0, 4);
-        GridPane.setHalignment(fix121Checkbox, HPos.LEFT);  
-        //  GridPane.setConstraints(progressBar, 0, 2, 2, 1);
-
-        ColumnConstraints col1 = new ColumnConstraints();
-        col1.setPercentWidth(60);
-        inputGridPane.getColumnConstraints().addAll(col1);
-        
-        inputGridPane.setHgap(6);
-        inputGridPane.setVgap(6);
-        inputGridPane.getChildren().addAll(label, openFilesButton, bookMenu, listVBox, openFilesCheckbox, fix121Checkbox, convertButton);
- 
-        final Pane rootGroup = new VBox(12);
-        rootGroup.getChildren().addAll(inputGridPane);
-        rootGroup.setPadding( new Insets(12, 12, 12, 12) );
- 
-        stage.setScene( new Scene(rootGroup, 300, 250) );
+        stage.setScene( scene ); // 305 for screenshots
         stage.show();
+
     }
  
     public static void main(String[] args) {
